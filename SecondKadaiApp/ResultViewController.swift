0eb97a7189c9fecd966bec3cb4dee7a9505efabd
tatsu.maki <tatsuya.spring0414@gmail.com>
2022-12-22 07:43:50 +0900

    画面遷移追加

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var result:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        label.text = "こんにちは、\(result)さん"
     }
     
 
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func unwind(_ sender: UIStoryboardSegue) {
+        
+    }
 }
 
